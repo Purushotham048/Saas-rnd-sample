f154eab49fb5dd597c30455785160cdcbbd37e59
purushotham-ux <shiva7601076032@gmail.com>
2025-01-29 17:23:55 +0530

    Adding new .env and requirements

--- src/home/settings.py
+++ src/home/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -18,11 +18,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p0g^x&8%d3spz%s-ru!h=lx_x1e@sq5sx%27hz$gvc1c0==@y2'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true"
+#DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true"
 
+DEBUG = config("DJANGO_DEBUG", cast = bool)
 print("Hello production server", DEBUG, type(DEBUG))
 
 ALLOWED_HOSTS = [
